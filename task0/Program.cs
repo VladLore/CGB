efaf06a9e1897ae5723e652611e5aff618fe6bcd
VladLore <vladiknikishin@gmail.com>
2023-06-10 11:21:05 +0300

    First commit

---
+++ task0/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int quater = number * number;
+// Console.WriteLine($"Квадрат числа {number} равен {quater}");
+
+Console.WriteLine("Введите первое яисло");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int two = Convert.ToInt32(Console.ReadLine());
+if (first*first==two) 
+Console.WriteLine("Второе число, явялется квадратом первого");
+else 
+Console.WriteLine("Первое число не явялется квадратом второго");
+
+Console.WriteLine("Введите число от 1 до 7");
+int user = Convert.ToInt32(Console.ReadLine());
+
+if (user==1)
+Console.WriteLine("Понедельник");
+else if (user == 2)
+Console.WriteLine("Вторник");
