dec37990edd2c3eae22e5982dff7252f427cb395
VladLore <vladiknikishin@gmail.com>
2023-06-11 18:12:40 +0300

    Решение задания 3

---
+++ HW1/work3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2 ==0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
