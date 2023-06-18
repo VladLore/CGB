241cc58ddcfd29e674f022bf237d1935865579c0
VladLore <vladiknikishin@gmail.com>
2023-06-18 22:58:43 +0300

    Решение 2 задачи

---
+++ HW2/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(SecNum(number));
+}
+
+
+int SecNum(int a)
+{
+    
+    int result = (a % 10);
+    return result;
+}
